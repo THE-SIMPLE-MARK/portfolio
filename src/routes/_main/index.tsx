8f104adabcc49bec91796ccbc81bbe65f7cd3469
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2025-11-08 16:41:24 +0100

    feat(home): use fuma docs button for home page blog CTA

--- src/routes/_main/index.tsx
+++ src/routes/_main/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { buttonVariants } from "~/components/ui/button";
 
 export const Route = createFileRoute("/_main/")({
 	component: Home,
@@ -6,20 +7,21 @@ export const Route = createFileRoute("/_main/")({
 
 function Home() {
 	return (
-		<div className="text-center">
-			<h1 className="font-medium text-5xl mb-4 font-handwritten">
+		<div className="text-center space-y-4">
+			<h1 className="font-medium text-5xl font-handwritten">
 				This is Márk Böszörményi.
 			</h1>
 			<p>
 				This is going to be my portfolio kind of website soon™.
 				<br /> For now, you can check out my blog {";)"}
 			</p>
+
 			<Link
+				className={buttonVariants({ color: "primary" })}
 				to="/blog/$"
 				params={{
 					_splat: "",
 				}}
-				className="mt-4 inline-block px-3 py-2 rounded-lg bg-fd-primary text-fd-primary-foreground font-medium text-sm mx-auto"
 			>
 				Open Blog
 			</Link>
