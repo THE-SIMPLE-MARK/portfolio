0a48ccfc20114defe259a15e78d275a3c2e824ff
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2025-11-09 00:07:11 +0100

    feat: reorder nitro config & remove "TODO" from temp fix comment

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,10 +1,10 @@
 export default defineNitroConfig({
 	compatibilityDate: "latest",
 	publicAssets: [
-		// TODO: this is a temporary fix to https://github.com/TanStack/router/issues/5368
+		// this is a temporary fix to https://github.com/TanStack/router/issues/5368
 		{
-			baseURL: "/__tsr",
 			dir: "dist/client/__tsr",
+			baseURL: "/__tsr",
 		},
 	],
 });
