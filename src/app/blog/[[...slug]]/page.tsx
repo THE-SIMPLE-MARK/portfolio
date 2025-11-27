1e4c8467525c1ad41bb72ff3c84a4f010abd7e9f
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2025-11-27 22:26:28 +0100

    feat: add LLM copy & share button

--- src/app/blog/[[...slug]]/page.tsx
+++ src/app/blog/[[...slug]]/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "fumadocs-ui/page"
 import type { Metadata } from "next"
 import { notFound } from "next/navigation"
+import { LLMCopyButton, ViewOptions } from "~/components/pageActions"
 import { getMDXComponents } from "~/lib/mdx/mdxComponents"
 import { source } from "~/lib/mdx/source"
 
@@ -20,11 +21,26 @@ export default async function Page({ params }: PageProps) {
 	if (!page) notFound()
 
 	const MDX = page.data.body
+	const markdownPath = slug ? slug.join("/") : "index"
+	const githubUrl = `https://github.com/THE-SIMPLE-MARK/portfolio/blob/main/content/blog/${markdownPath}.mdx`
 
 	return (
 		<DocsPage toc={page.data.toc} full={page.data.full}>
 			<DocsTitle>{page.data.title}</DocsTitle>
-			<DocsDescription>{page.data.description}</DocsDescription>
+			<DocsDescription className="mb-0">
+				{page.data.description}
+			</DocsDescription>
+
+			<div
+				id="page-actions"
+				className="flex flex-row gap-2 items-center border-b pb-6"
+			>
+				<LLMCopyButton markdownUrl={`/blog/markdown/${markdownPath}`} />
+				<ViewOptions
+					markdownUrl={`/blog/markdown/${markdownPath}`}
+					githubUrl={githubUrl}
+				/>
+			</div>
 
 			<DocsBody>
 				<MDX
