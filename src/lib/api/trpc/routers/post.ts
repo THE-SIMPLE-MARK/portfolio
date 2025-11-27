bd3e453ea3f2b8fae8a652bb87c755def32497c8
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2025-11-27 19:46:18 +0100

    fix: remove prisma usages

--- src/lib/api/trpc/routers/post.ts
+++ src/lib/api/trpc/routers/post.ts
@@ -2,7 +2,6 @@ import { z } from "zod"
 
 import { createTRPCRouter } from "~/lib/api/trpc"
 import { publicProcedure } from "~/lib/api/trpc/procedures/public"
-import prisma from "~/lib/api/prisma"
 
 export const postRouter = createTRPCRouter({
 	hello: publicProcedure
@@ -16,18 +15,13 @@ export const postRouter = createTRPCRouter({
 	create: publicProcedure
 		.input(z.object({ name: z.string().min(1) }))
 		.mutation(async ({ input }) => {
-			return prisma.post.create({
-				data: {
-					name: input.name,
-				},
-			})
+			return {
+				id: "1",
+				name: input.name,
+			}
 		}),
 
 	getLatest: publicProcedure.query(async () => {
-		const post = await prisma.post.findFirst({
-			orderBy: { createdAt: "desc" },
-		})
-
-		return post ?? null
+		return null
 	}),
 })
